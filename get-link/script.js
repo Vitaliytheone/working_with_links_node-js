d2d39373136f84b397000ed5808b6419bbf9d3f0
Vitaliy <Vitaliythesecond@gmail.com>
2022-07-29 23:02:33 +0300

    get link

---
+++ get-link/script.js
@@ -0,0 +1,9 @@
+const links = {
+    yandex: "https://yandex.ru/",
+    google: "https://www.google.com/",
+    spotify: "https://www.spotify.com/",
+};
+
+const alias = "yandex";
+
+console.log(links[alias]);
